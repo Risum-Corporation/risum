50a22a3cfa59cbe1700dc9d701c777e07ffefa09
Educg550 <eduardo9cruz@outlook.com>
2021-04-08 14:06:51 -0300

    Protótipo da primeira tela e ajuste nas variáveis de cores

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,66 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonButton,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from "@ionic/react";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
-    <IonPage>
+    <IonPage className="page">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Bem-vindo!</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Teste</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonGrid>
+          <IonRow>
+            <IonImg src={"assets/icon/icon.svg"} />
+          </IonRow>
+          <IonRow>
+            <IonTitle className="title" color="primary">Risum</IonTitle>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton
+                color="secondary "
+                size="large"
+                id="signup-button"
+                className="button"
+              >
+                Cadastrar
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton
+                size="large"
+                id="login-button"
+                className="button"
+                fill="outline"
+              >
+                Login
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonButton color="tertiary" expand="block" fill="outline">
+              Entrar como convidado
+            </IonButton>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -7,7 +7,7 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
