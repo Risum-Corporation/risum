0b63d0a767eae87b71861a0af5fe13b6e017b0df
Educg550 <eduardo9cruz@outlook.com>
2021-04-12 14:00:06 -0300

    CSS identation and code optimization

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,42 @@
 import {
-    IonContent,
-    IonButton,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonImg,
-    IonLabel,
-  } from "@ionic/react";
-  import "./Home.css";
-  
-  const Home: React.FC = () => {
-    return (
-      <IonPage className="page">
-        <IonContent fullscreen>
-          <IonImg src={"assets/icon/icon.svg"} className="img" />
-  
-          <IonTitle className="title" color="primary"> Risum </IonTitle>
-  
-          <IonButton color="secondary" id="signup-button" className="button" >Criar<br />conta</IonButton>
-  
-          <IonButton color="primary" id="login-button" className="button">Login</IonButton>
-  
-          <IonButton
+  IonContent,
+  IonButton,
+  IonPage,
+  IonTitle,
+  IonImg,
+  IonLabel,
+} from "@ionic/react";
+import "./Home.css";
 
-            className="button" id="guest-button">Entrar como convidado </IonButton>
+const Home: React.FC = () => {
+  return (
+    <IonPage className="page">
+      <IonContent fullscreen>
+        <IonImg src={"assets/icon/icon.svg"} className="img" />
 
-          <IonLabel className="footer">Bem vindo ao Risum!</IonLabel>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Home;
-  
+        <IonTitle className="title" color="primary">
+          {" "}
+          Risum{" "}
+        </IonTitle>
+
+        <IonButton color="secondary" id="signup-button" className="button">
+          Criar
+          <br />
+          conta
+        </IonButton>
+
+        <IonButton color="primary" id="login-button" className="button">
+          Login
+        </IonButton>
+
+        <IonButton className="button" id="guest-button">
+          Entrar como convidado{" "}
+        </IonButton>
+
+        <IonLabel className="footer">Bem vindo ao Risum!</IonLabel>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
