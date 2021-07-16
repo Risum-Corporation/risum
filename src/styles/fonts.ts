6d30efb3f8a0cd88f469d2b8e0be29796954b65c
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-07-16 17:43:35 -0300

    remove bugs

--- src/styles/fonts.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    heading: 'Archivo_700Bold',
-    subtitle: 'Archivo_500Medium',
-    text: 'Roboto_400Regular',
-    userText: 'Poppins_600SemiBold',
-    complement: 'Poppins_400Regular',
-};
