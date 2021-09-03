807c11fc9707ebe07f1526d3a0c03f957302bde1
Educg550 <eduardo9cruz@gmail.com>
2021-09-03 10:01:42 -0300

    Drawer style prototype

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,15 +1,53 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import {
   DrawerContentScrollView,
   DrawerItemList,
 } from "@react-navigation/drawer";
 
+import colors from "../styles/colors";
+
+import { Ionicons } from "@expo/vector-icons";
+import fonts from "../styles/fonts";
+
 export function Drawer(props) {
   return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-      <Text>Drawer Estilizado!</Text>
-    </DrawerContentScrollView>
+    <View style={styles.container}>
+      <DrawerContentScrollView {...props}>
+        <View style={styles.wrapper}>
+          <View style={styles.icons}>{/* Ionicons */}</View>
+          <View style={styles.itemList}>
+            <DrawerItemList
+              {...props}
+              itemStyle={styles.item}
+              activeBackgroundColor={colors.purple}
+              inactiveBackgroundColor={colors.lightBackground}
+            />
+          </View>
+        </View>
+
+        <Text>Drawer Estilizado!</Text>
+      </DrawerContentScrollView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    width: "100%",
+  },
+  wrapper: {
+    flexDirection: "column",
+  },
+  icons: {
+    marginVertical: 5,
+  },
+  itemList: {
+    marginVertical: 5,
+  },
+  item: {
+    color: colors.white,
+  },
+});
